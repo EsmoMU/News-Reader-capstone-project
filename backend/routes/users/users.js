9d67b5a1d4ce5a9cd4c7a671bea8051ec2093142
Milad Youssef <milad.youssef@libertymutual.com>
2025-07-21 19:16:22 -0400

    users GET endpoint working

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -1,17 +1,22 @@
 import express from "express";
+import { client, connect } from "./db.js";
 const router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const db = client.db(dbName);
-//     const collection = db.collection(collectionName);
-//     const prj = { user: 1, email: 1, _id: 0 };
-//     const users = await collection.find({}).project(prj).toArray();
-//     res.json(users);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+const dbName = "usersdb";
+const collectionName = "users";
+connect(); // Connect to MongoDB
+
+router.get("/", async (req, res) => {
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const prj = { user: 1, email: 1, _id: 0 };
+    const users = await collection.find({}).project(prj).toArray();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 // router.post("/authenticate", async (req, res) => {
 //   try {
